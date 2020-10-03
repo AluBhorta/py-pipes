0a1d955659511969bb6e0f1f91d6459e372103c0
alu@ubuntu20-ZeDHP <dev.bhorta@gmail.com>
2020-10-03 13:15:32 +0600

    refactor and reformat

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import time
-import os, signal
+import os
+import signal
 from multiprocessing import Process, Pipe
 from threading import Thread
 
 CLOSE_MESSAGE = "\n\n"
 
-def child(conn, length=10, sleep_for_secs=1):
+
+def child_func(conn, length=10, sleep_for_secs=1):
     cpid = os.getpid()
     print(f"child pid: {cpid}")
     for i in range(length):
@@ -18,9 +20,9 @@ def child(conn, length=10, sleep_for_secs=1):
     conn.send(CLOSE_MESSAGE)
     conn.close()
     os.kill(cpid, signal.SIGTERM)
-    
 
-def main_reader(read_pipe):
+
+def pipe_reader(read_pipe):
     while True:
         try:
             val = read_pipe.recv()
@@ -31,41 +33,41 @@ def main_reader(read_pipe):
             print(e)
 
 
-def main(num_of_workers=3, length=10, sleep_for_secs=1):
+def parent_func(num_of_children=3, length=10, sleep_for_secs=1):
     print(f"parent pid: {os.getpid()}")
 
-    workers = []
-
-    for i in range(num_of_workers):
+    children = []
+    for i in range(num_of_children):
         read_conn, write_conn = Pipe()
-        p = Process(target=child, args=(write_conn,))
-        workers.append({
-            "pipes": (read_conn,write_conn),
-            "process": Process(target=child, args=(write_conn,length, sleep_for_secs))
+        children.append({
+            "pipes": (read_conn, write_conn),
+            "process": Process(
+                target=child_func,
+                args=(write_conn, length, sleep_for_secs)
+            )
         })
 
     readers = []
-    for w in workers:
-        w["process"].start()
-        
-        t = Thread(target=main_reader, args=(w["pipes"][0],))
+    for child in children:
+        child["process"].start()
+        t = Thread(target=pipe_reader, args=(child["pipes"][0],))
         t.start()
         readers.append(t)
 
-    for r in readers:
-        r.join()
-    
-    for w in workers:
-        w["pipes"][0].close()
-        w["process"].join()
-        w["process"].close()
-        w["process"].close()
+    for reader in readers:
+        reader.join()
+
+    for child in children:
+        child["pipes"][0].close()
+        child["process"].join()
+        child["process"].close()
+        child["process"].close()
 
 
 if __name__ == '__main__':
-    num_of_workers = int(input("Enter number of workers (int): "))
+    num_of_children = int(input("Enter number of workers (int): "))
     length = int(input("Enter length of work i.e. how long to work (int): "))
-    sleep_for_secs = float(input("Enter sleep interval in seconds for each worker (float): "))
+    sleep_for_secs = float(
+        input("Enter sleep interval in seconds for each worker (float): "))
 
-    main(num_of_workers, length, sleep_for_secs)
-    
+    parent_func(num_of_children, length, sleep_for_secs)
